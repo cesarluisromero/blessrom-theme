452bbe32ad2fa23fe228836e647e3b193fa1e084
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 18:23:45 -0500

    diseño

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -8,23 +8,21 @@
         @php $ids = array_merge([$main_image], $attachment_ids); @endphp
         @foreach ($ids as $id)
             <div class="swiper-slide">
-                <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
+                <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain lg:hidden mb-6">
             </div>
         @endforeach
     </div>
-    <div class="swiper-pagination absolute bottom-1 inset-x-0 flex justify-center"></div>
-
- 
     
+    <div class="swiper-pagination absolute bottom-1 inset-x-0 flex justify-center"></div>   
 </div>
-   <div class="px-4 py-4 space-y-3 lg:hidden mb-6">
-        {{-- Precio --}}
-        <div class="text-center text-xl font-bold text-blue-600">
-            {!! $product->get_price_html() !!}
-        </div>
-
-        {{-- Atributos --}}
-        <div class="text-sm text-center text-gray-700">
-            {!! woocommerce_template_single_add_to_cart() !!}
-        </div>
+<div class="px-4 py-4 space-y-3 lg:hidden mb-6">
+    {{-- Precio --}}
+    <div class="text-center text-xl font-bold text-blue-600">
+        {!! $product->get_price_html() !!}
     </div>
+
+    {{-- Atributos --}}
+    <div class="text-sm text-center text-gray-700">
+        {!! woocommerce_template_single_add_to_cart() !!}
+    </div>
+</div>
