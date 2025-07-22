3987ed3b5cd6c10c8f8f32c3074e1dd3908fcdcd
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 16:41:05 -0500

    diseño

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -1,9 +1,9 @@
-<div x-data="productGallery()" class="swiper-container product-swiper block lg:hidden mb-6">
+<div x-data="productGallery()" class="swiper product-swiper block lg:hidden mb-6">
     <div class="swiper-wrapper">
         @php $ids = array_merge([$main_image], $attachment_ids); @endphp
         @foreach ($ids as $id)
             <div class="swiper-slide">
-                <img src="{{ wp_get_attachment_image_url($id, 'medium') }}" class="w-full h-auto object-contain">
+                <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
             </div>
         @endforeach
     </div>
