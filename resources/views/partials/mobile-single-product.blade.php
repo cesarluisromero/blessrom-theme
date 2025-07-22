5f37b116ff6a05bcf8e6f6c2a4d813cb5e9511e7
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 16:32:22 -0500

    diseño

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -3,21 +3,16 @@
       style='font-family: "Plus Jakarta Sans", "Noto Sans", sans-serif;'
     >
     <div>
-        <div class="@container">
-          <div class="@[480px]:px-4 @[480px]:py-3">
-            <div
-              class="bg-cover bg-center flex flex-col justify-end overflow-hidden bg-white @[480px]:rounded-lg min-h-80"
-              style='background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0) 25%), url("https://lh3.googleusercontent.com/aida-public/AB6AXuD2LtpR5utVRSB6emShNNcCatKhW-YAh80HzQdv3Eu6vZebIIqi7M3LZkfMufoGPoWMJ6lD0XVgQu2QKOm3wnLcvhgqx5mk-3DKqohfwriZxpie9V_AI1SWkFkg7_98WMFdYp1ZXz5v01-WgaXt0gLsJRwwsq-9y1_BDShnn2zT-Uc-m0p0fzaZ_YQx4bnXwp05cxq_slxdvz4krkMBtAIj4smaBLHSebPe5MTpd8JrUtn5X-oVE35GMzQNqKAAN3qaXs333AOfMoRA");'
-            >
-              <div class="flex justify-center gap-2 p-5">
-                <div class="size-1.5 rounded-full bg-white"></div>
-                <div class="size-1.5 rounded-full bg-white opacity-50"></div>
-                <div class="size-1.5 rounded-full bg-white opacity-50"></div>
-                <div class="size-1.5 rounded-full bg-white opacity-50"></div>
-                <div class="size-1.5 rounded-full bg-white opacity-50"></div>
-              </div>
+        <div x-data="productGallery()" class="swiper-container product-swiper block lg:hidden mb-6">
+            <div class="swiper-wrapper">
+                @php $ids = array_merge([$main_image], $attachment_ids); @endphp
+                @foreach ($ids as $id)
+                    <div class="swiper-slide">
+                        <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
+                    </div>
+                @endforeach
             </div>
-          </div>
+            <div class="swiper-pagination mt-2"></div>
         </div>
     </div>
 </div>
