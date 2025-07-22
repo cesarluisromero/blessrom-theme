52675670e5a4f282e49ed81d01ca0f0e6f6b608d
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 16:33:43 -0500

    disñeo

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -1,18 +1,11 @@
-<div
-      class="relative flex size-full min-h-screen flex-col bg-white justify-between group/design-root overflow-x-hidden"
-      style='font-family: "Plus Jakarta Sans", "Noto Sans", sans-serif;'
-    >
-    <div>
-        <div x-data="productGallery()" class="swiper-container product-swiper block lg:hidden mb-6">
-            <div class="swiper-wrapper">
-                @php $ids = array_merge([$main_image], $attachment_ids); @endphp
-                @foreach ($ids as $id)
-                    <div class="swiper-slide">
-                        <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
-                    </div>
-                @endforeach
+<div x-data="productGallery()" class="swiper-container product-swiper block lg:hidden mb-6">
+    <div class="swiper-wrapper">
+        @php $ids = array_merge([$main_image], $attachment_ids); @endphp
+        @foreach ($ids as $id)
+            <div class="swiper-slide">
+                <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
             </div>
-            <div class="swiper-pagination mt-2"></div>
-        </div>
+        @endforeach
     </div>
+    <div class="swiper-pagination mt-2"></div>
 </div>
