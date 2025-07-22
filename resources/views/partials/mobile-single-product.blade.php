12bf719c498faf50827da621e89f18f77bdad7e1
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 18:12:22 -0500

    diseño

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -1,30 +1,29 @@
-<div class="block lg:hidden mb-6">
 
+
+<div x-data="productGallery()" class="product-swiper swiper block lg:hidden mb-6">
     <div class="text-center text-lg font-semibold text-gray-800">
-            {{ $product->get_name() }}
+        {{ $product->get_name() }}
     </div>
-    <div x-data="productGallery()" class="product-swiper swiper>
-        <div class="swiper-wrapper">
-            @php $ids = array_merge([$main_image], $attachment_ids); @endphp
-            @foreach ($ids as $id)
-                <div class="swiper-slide">
-                    <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
-                </div>
-            @endforeach
-        </div>
-        <div class="swiper-pagination absolute bottom-1 inset-x-0 flex justify-center"></div>
-        
+    <div class="swiper-wrapper">
+        @php $ids = array_merge([$main_image], $attachment_ids); @endphp
+        @foreach ($ids as $id)
+            <div class="swiper-slide">
+                <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
+            </div>
+        @endforeach
     </div>
+    <div class="swiper-pagination absolute bottom-1 inset-x-0 flex justify-center"></div>
+
     <div class="px-4 py-4 space-y-3">
-            {{-- Precio --}}
-            <div class="text-center text-xl font-bold text-blue-600">
-                {!! $product->get_price_html() !!}
-            </div>
+        {{-- Precio --}}
+        <div class="text-center text-xl font-bold text-blue-600">
+            {!! $product->get_price_html() !!}
+        </div>
 
-            {{-- Atributos --}}
-            <div class="text-sm text-center text-gray-700">
-                {!! woocommerce_template_single_add_to_cart() !!}
-            </div>
+        {{-- Atributos --}}
+        <div class="text-sm text-center text-gray-700">
+            {!! woocommerce_template_single_add_to_cart() !!}
+        </div>
     </div>
-
+    
 </div>
