5f5c640a498effb336fc9d1a4e36557cb907f0c7
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 18:05:49 -0500

    diseño

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -1,7 +1,8 @@
-<div class="text-center text-lg font-semibold text-gray-800">
+<div x-data="productGallery()" class="swiper-container product-swiper block lg:hidden mb-6">
+    {{-- Encabezado móvil con título del producto --}}
+    <div class="text-center text-lg font-semibold text-gray-800">
         {{ $product->get_name() }}
-</div>
-<div x-data="productGallery()" class="product-swiper swiper block lg:hidden mb-6">
+    </div>
     <div class="swiper-wrapper">
         @php $ids = array_merge([$main_image], $attachment_ids); @endphp
         @foreach ($ids as $id)
@@ -10,10 +11,9 @@
             </div>
         @endforeach
     </div>
-    <div class="swiper-pagination absolute bottom-1 inset-x-0 flex justify-center"></div>
-    
-</div>
-<div class="px-4 py-4 space-y-3">
+    <div class="swiper-pagination mt-2"></div>
+
+    <div class="px-4 py-4 space-y-3">
         {{-- Precio --}}
         <div class="text-center text-xl font-bold text-blue-600">
             {!! $product->get_price_html() !!}
@@ -23,4 +23,7 @@
         <div class="text-sm text-center text-gray-700">
             {!! woocommerce_template_single_add_to_cart() !!}
         </div>
+    </div>
 </div>
+
+
