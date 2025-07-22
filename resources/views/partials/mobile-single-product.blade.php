a5a90323f000371c266a54085a394b5a6359aa49
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 16:36:59 -0500

    diseño

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -3,7 +3,7 @@
         @php $ids = array_merge([$main_image], $attachment_ids); @endphp
         @foreach ($ids as $id)
             <div class="swiper-slide">
-                <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
+                <img src="{{ wp_get_attachment_image_url($id, 'medium') }}" class="w-full h-auto object-contain">
             </div>
         @endforeach
     </div>
