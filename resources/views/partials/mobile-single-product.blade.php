460d0ef174a96e0040b505c14cb0f68eb2383910
Cesar Luis RR <romcel87@gmail.com>
2025-07-22 15:53:33 -0500

    diseño single

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -1,17 +1,21 @@
-<div x-data="productGallery()" class="swiper-container product-swiper block lg:hidden mb-6">
+<div x-data="productGallery()" class="block lg:hidden">
     {{-- Encabezado móvil con título del producto --}}
     <div class="text-center text-lg font-semibold text-gray-800">
         {{ $product->get_name() }}
     </div>
-    <div class="swiper-wrapper">
-        @php $ids = array_merge([$main_image], $attachment_ids); @endphp
-        @foreach ($ids as $id)
-            <div class="swiper-slide">
-                <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-auto object-contain">
-            </div>
-        @endforeach
+    <div class="swiper-container product-swiper aspect-[3/4] bg-cover bg-center rounded-lg overflow-hidden">
+    
+        <div class="swiper-wrapper">
+            @php $ids = array_merge([$main_image], $attachment_ids); @endphp
+            @foreach ($ids as $id)
+                <div class="swiper-slide">
+                    <img src="{{ wp_get_attachment_image_url($id, 'large') }}" class="w-full h-full object-cover">
+                </div>
+            @endforeach
+        </div>
+         <div class="flex justify-center gap-2 py-4 swiper-pagination"></div>
     </div>
-    <div class="swiper-pagination mt-2"></div>
+    
 
     <div class="px-4 py-4 space-y-3">
         {{-- Precio --}}
