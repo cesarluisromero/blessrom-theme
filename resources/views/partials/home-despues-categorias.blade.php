1c7a8ed444450564ca66fc355680f6f57a6505e8
Cesar Luis RR <romcel87@gmail.com>
2025-07-24 12:09:20 -0500

    diseño desktop

--- resources/views/partials/home-despues-categorias.blade.php
+++ resources/views/partials/home-despues-categorias.blade.php
@@ -1,3 +1,7 @@
-<section class="hidden md:block w-full">
+<section class="py-2">
+  <div class="container mx-auto px-4">
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
   {!! do_shortcode('[smartslider3 slider="5"]') !!}
+  </div>
+  </div>
 </section>
