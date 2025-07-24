88ab2fcba6a5729b4e67b7d80ef916fd89378555
Cesar Luis RR <romcel87@gmail.com>
2025-07-24 12:19:34 -0500

    home

--- resources/views/partials/home-categories.blade.php
+++ resources/views/partials/home-categories.blade.php
@@ -35,5 +35,8 @@
       </a>
 
     </div>
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+      {!! do_shortcode('[smartslider3 slider="5"]') !!}
+    </div>
   </div>
 </section>
--- resources/views/partials/home-despues-categorias.blade.php
+++ resources/views/partials/home-despues-categorias.blade.php
@@ -1,7 +1,7 @@
 <section class="py-2">
   <div class="container mx-auto px-4">
     <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-  {!! do_shortcode('[smartslider3 slider="5"]') !!}
+        {!! do_shortcode('[smartslider3 slider="5"]') !!}
   </div>
   </div>
 </section>
