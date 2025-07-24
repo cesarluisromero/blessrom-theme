fdac64a9618da8cd8f8e7c2418a0943a55b1bcd8
Cesar Luis RR <romcel87@gmail.com>
2025-07-24 13:32:37 -0500

    condicional

--- resources/views/partials/home-categories.blade.php
+++ resources/views/partials/home-categories.blade.php
@@ -31,15 +31,16 @@ function get_random_product_image_from_category($category_id) {
   <div class="container mx-auto px-4">
     <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
       @foreach($categories as $cat)
-        @php
-          $image = get_random_product_image_from_category($cat->term_id);
-          $cat_link = get_term_link($cat);
-        @endphp
-      
+        @if($cat->name === 'chavo' || $cat->name === 'faldas'|| $cat->name === 'Short')
+            @php
+              $image = get_random_product_image_from_category($cat->term_id);
+              $cat_link = get_term_link($cat);
+            @endphp
+          @endif
         <a href="{{ $cat_link }}" class="bg-white rounded-2xl shadow-md hover:shadow-lg transition duration-300 p-15 flex flex-col items-center text-center">
           <img src="{{ $image }}" alt="{{ $cat->name }}" class="rounded-xl w-64 h-64 object-contain mb-4 transition-transform duration-300 hover:scale-105" />
           <h3 class="text-5xl font-semibold text-gray-700 mb-1 font-serif">{{ $cat->name }}</h3>
-          
+          <p class="text-sm text-gray-500 mb-3">{{ $cat->description ?: 'Productos destacados' }}</p>
           <span class="inline-block bg-[#FFB816] text-white text-lg font-semibold px-6 py-2 rounded-full hover:bg-yellow-500 transition">
              Ver más
           </span>
