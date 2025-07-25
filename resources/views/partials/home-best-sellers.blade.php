24c4690143b3aea78f1b0daf55b60de79f7cbd06
Cesar Luis RR <romcel87@gmail.com>
2025-07-25 10:39:55 -0500

    text formato best seller

--- resources/views/partials/home-best-sellers.blade.php
+++ resources/views/partials/home-best-sellers.blade.php
@@ -19,8 +19,8 @@
     class="w-full h-full object-cover"
   />        
           </div>
-          <h3 class="text-lg font-semibold mb-2 font-serif">{{ $producto->get_name() }}</h3>
-          <p class="text-black font-bold text-sm">{!! $producto->get_price_html() !!}</p>
+          <h3 class="text-lg mb-2 font-serif">{{ $producto->get_name() }}</h3>
+          <p class= "font-serif text-sm">{!! $producto->get_price_html() !!}</p>
           <span class="mt-2 inline-block bg-[#FFB816] text-white text-sm font-semibold py-2 px-4 rounded hover:bg-yellow-500 transition">
             Ver producto
           </span>
