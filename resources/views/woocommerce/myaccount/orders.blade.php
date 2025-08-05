bb177f3dc09f85fcb80da44bae4bf473e7b66b2d
Cesar Luis RR <romcel87@gmail.com>
2025-08-05 17:49:02 -0500

    orders

--- resources/views/woocommerce/myaccount/orders.blade.php
+++ resources/views/woocommerce/myaccount/orders.blade.php
@@ -5,7 +5,17 @@
         do_action( 'woocommerce_before_account_orders', $has_orders ); 
     @endphp
 
-    @php
-        if ($has_orders) :
-    @endphp
+    @if ($has_orders)
+        <table class="woocommerce-orders-table woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table">
+        </table>
+        @php 
+            do_action( 'woocommerce_before_account_orders_pagination' );
+        @endphp
+
+       
+
+    @else
+
+
+    @endif
 @endsection
