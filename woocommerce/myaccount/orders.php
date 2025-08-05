7d270d8fb643fbadc1a345f47088765bacedac40
Cesar Luis RR <romcel87@gmail.com>
2025-08-05 17:16:29 -0500

    orders

--- woocommerce/myaccount/orders.php
+++ woocommerce/myaccount/orders.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * Orders
- *
- * Shows orders on the account page.
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/orders.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see https://woocommerce.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 9.5.0
- */
 
 defined( 'ABSPATH' ) || exit;
 
@@ -26,6 +9,7 @@ do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
 	<table class="woocommerce-orders-table woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table">
 		<thead>
 			<tr>
+                <a>ddsds</a>
 				<?php foreach ( wc_get_account_orders_columns() as $column_id => $column_name ) : ?>
 					<th scope="col" class="woocommerce-orders-table__header woocommerce-orders-table__header-<?php echo esc_attr( $column_id ); ?>"><span class="nobr"><?php echo esc_html( $column_name ); ?></span></th>
 				<?php endforeach; ?>
