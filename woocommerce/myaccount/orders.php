2ef56629bacb378605150ff3ca500580ced5d093
Cesar Luis RR <romcel87@gmail.com>
2025-08-05 18:23:25 -0500

    orders

--- woocommerce/myaccount/orders.php
+++ woocommerce/myaccount/orders.php
@@ -1,7 +1,26 @@
 <?php
-
 defined( 'ABSPATH' ) || exit;
 
-echo \Roots\view('woocommerce.myaccount.orders')->render();
+/**
+ * Réplica de la lógica del template nativo.
+ * Ajusta si necesitas otros filtros.
+ */
+$current_page    = max( 1, absint( get_query_var( 'paged' ) ) );
+$customer_orders = wc_get_orders(
+    apply_filters(
+        'woocommerce_my_account_my_orders_query',
+        [
+            'customer' => get_current_user_id(),
+            'page'     => $current_page,
+            'paginate' => true,
+        ]
+    )
+);
 
+$has_orders = $customer_orders->total > 0;   // ← la variable que necesitas
 
+echo \Roots\view('woocommerce.myaccount.orders', [
+    'customer_orders' => $customer_orders,
+    'has_orders'      => $has_orders,
+    'current_page'    => $current_page,
+])->render();
