eda33338e9e1b9c381da1123b958ed8d4d064912
Cesar Luis RR <romcel87@gmail.com>
2025-08-05 16:21:32 -0500

    scrollbar

--- resources/views/partials/home-slider-products.blade.php
+++ resources/views/partials/home-slider-products.blade.php
@@ -22,14 +22,12 @@
 
         <div class="swiper-button-next product-swiper-button-next !hidden md:!flex text-blue-500 absolute right-0 top-1/2 -translate-y-1/2 z-10 w-10 h-10 items-center justify-center bg-white rounded-full shadow-md"></div>
 
-     
-
-        <div class="swiper-pagination product-swiper-pagination block md:hidden mt-4"></div>
-
-
-
         <!-- Barra inferior -->
-        <div class="swiper-scrollbar"></div>
+        <div
+            class="swiper-scrollbar h-1.5 rounded-full bg-gray-200/60
+                  hover:bg-gray-300/70 transition-colors duration-200"
+          ></div>
+
       </div>
     </div>
   </div>
