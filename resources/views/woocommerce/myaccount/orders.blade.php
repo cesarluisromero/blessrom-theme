4e99a98f572d0847e07e20d61537cbf356176e29
Cesar Luis RR <romcel87@gmail.com>
2025-08-05 19:07:21 -0500

    do

--- resources/views/woocommerce/myaccount/orders.blade.php
+++ resources/views/woocommerce/myaccount/orders.blade.php
@@ -30,7 +30,7 @@
                             $item_count = $order->get_item_count() - $order->get_item_count_refunded();
                         @endphp
 
-                        <tr class="odd:bg-white even:bg-gray-50 woocommerce-orders-table__row woocommerce-orders-table__row--status-{{ esc_attr($order->get_status()) }} order">
+                        <tr class="odd:bg-white even:bg-blue-300 woocommerce-orders-table__row woocommerce-orders-table__row--status-{{ esc_attr($order->get_status()) }} order">
                             @foreach (wc_get_account_orders_columns() as $column_id => $column_name)
                                 @php $is_order_number = $column_id === 'order-number'; @endphp
 
