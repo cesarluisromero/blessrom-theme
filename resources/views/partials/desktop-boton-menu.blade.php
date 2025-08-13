2cf5ebb8cf7b9efcfbbfc7ecc59d82f070c49333
Cesar Luis RR <romcel87@gmail.com>
2025-08-13 18:50:26 -0500

    color menu

--- resources/views/partials/desktop-boton-menu.blade.php
+++ resources/views/partials/desktop-boton-menu.blade.php
@@ -1,4 +1,4 @@
-<button @click="showMenu = !showMenu" class="bg-[#165DFF] border border-white text-white px-4 py-2 rounded inline-flex items-center">
+<button @click="showMenu = !showMenu" class="bg-[#111b2e] border border-white text-white px-4 py-2 rounded inline-flex items-center">
     <svg class="h-5 w-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
     </svg>
--- resources/views/partials/header-menu.blade.php
+++ resources/views/partials/header-menu.blade.php
@@ -10,7 +10,7 @@
 </div>
 
 {{-- Encabezado ESCRITORIO --}}
-<nav class="hidden lg:flex bg-[#101828] text-white shadow-md w-full m-0 p-0">
+<nav class="hidden lg:flex bg-[#111b2e] text-white shadow-md w-full m-0 p-0">
   <div class="flex flex-col w-full max-w-7xl px-4 py-3 mx-auto">
     <div class="flex items-center justify-between w-full">
       @include('partials.logo-blessrom')
