d634ca806949c5ecf0832dbce34cafc18e462909
Cesar Luis RR <romcel87@gmail.com>
2025-08-22 12:53:26 -0500

    columna 1

--- resources/views/partials/single-product-columna1.blade.php
+++ resources/views/partials/single-product-columna1.blade.php
@@ -1,20 +1,19 @@
-{{-- Columna 1: Imágenes --}}
-
 @php
-  // Armar lista de miniaturas (principal + adjuntas), sin nulos
-  $thumb_ids = array_values(array_filter(array_merge(
+  // Miniaturas = principal + adjuntas (filtra nulos)
+  $thumb_ids   = array_values(array_filter(array_merge(
     $main_image ? [$main_image] : [],
     $attachment_ids ?? []
   )));
-  $top_ids    = array_slice($thumb_ids, 0, 6);   // primeras 6
-  $bottom_ids = array_slice($thumb_ids, 6);      // el resto
+  $top_ids     = array_slice($thumb_ids, 0, 6);   // primeras 6
+  $bottom_ids  = array_slice($thumb_ids, 6);      // el resto
 @endphp
 
-<div class="grid grid-cols-[112px_1fr] grid-rows-[auto_auto] gap-3 items-start bg-white ml-4">
+{{-- Columna 1: Imágenes --}}
+<div class="grid grid-cols-[112px_1fr] gap-3 items-start bg-white ml-4">
 
-  {{-- Rail de miniaturas (col 1) dividido en 2 filas --}}
-  <div class="col-start-1 row-start-1">
-    {{-- Fila 1: primeras 6 (una debajo de otra) --}}
+  {{-- Izquierda: rail de miniaturas --}}
+  <div class="hidden lg:block">
+    {{-- Arriba: primeras 6 --}}
     <div class="flex flex-col gap-2">
       @foreach ($top_ids as $id)
         <img
@@ -23,12 +22,10 @@
           @click="$store.product.currentImage = '{{ wp_get_attachment_image_url($id, 'large') }}'">
       @endforeach
     </div>
-  </div>
 
-  {{-- Fila 2: resto de miniaturas (solo si hay más de 6) --}}
-  @if (count($bottom_ids))
-    <div class="col-start-1 row-start-2">
-      <div class="flex flex-col gap-2">
+    {{-- Abajo: resto (solo si hay más de 6) --}}
+    @if (count($bottom_ids))
+      <div class="mt-4 pt-4 border-t flex flex-col gap-2">
         @foreach ($bottom_ids as $id)
           <img
             src="{{ wp_get_attachment_image_url($id, 'thumbnail') }}"
@@ -36,12 +33,12 @@
             @click="$store.product.currentImage = '{{ wp_get_attachment_image_url($id, 'large') }}'">
         @endforeach
       </div>
-    </div>
-  @endif
+    @endif
+  </div>
 
-  {{-- Imagen principal (col 2, ocupa 2 filas) --}}
+  {{-- Derecha: imagen principal --}}
   <div
-    class="relative col-start-2 row-span-2"
+    class="relative"
     x-data="{
       zoomX: 0, zoomY: 0, showZoom: false,
       zoomEnabled: window.innerWidth >= 768,
@@ -49,8 +46,7 @@
         if (!this.zoomEnabled) return
         const b = e.target.getBoundingClientRect()
         const x = e.clientX - b.left, y = e.clientY - b.top
-        this.zoomX = -x * 1.5 + 350
-        this.zoomY = -y * 1.5 + 275
+        this.zoomX = -x * 1.5 + 350; this.zoomY = -y * 1.5 + 275
       }
     }"
     x-init="window.addEventListener('resize', () => zoomEnabled = window.innerWidth >= 768)"
@@ -71,7 +67,7 @@
     >
       <img
         :src="$store.product.currentImage"
-        :style="'transform: scale(1.5) translate(' + zoomX + 'px, ' + zoomY + 'px)'"
+        :style="'transform: scale(1.5) translate(' + zoomX + 'px,' + zoomY + 'px)'"
         class="w-full h-auto object-contain"
         alt="Zoom imagen"
       >
