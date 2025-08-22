a36e38c4cd79ba163ab4470a019dc9b433914909
Cesar Luis RR <romcel87@gmail.com>
2025-08-22 12:42:48 -0500

    columna 1

--- resources/views/partials/single-product-columna1.blade.php
+++ resources/views/partials/single-product-columna1.blade.php
@@ -1,5 +1,7 @@
+{{-- Columna 1: Imágenes --}}
+
 @php
-  // Armar lista de thumbs (incluye principal + adjuntas, sin nulos)
+  // Armar lista de miniaturas (principal + adjuntas), sin nulos
   $thumb_ids = array_values(array_filter(array_merge(
     $main_image ? [$main_image] : [],
     $attachment_ids ?? []
@@ -8,35 +10,36 @@
   $bottom_ids = array_slice($thumb_ids, 6);      // el resto
 @endphp
 
-{{-- Columna 1: Imágenes --}}
-<div class="grid grid-cols-[96px_1fr] grid-rows-[auto_auto] gap-3 items-start bg-white ml-4">
+<div class="grid grid-cols-[112px_1fr] grid-rows-[auto_auto] gap-3 items-start bg-white ml-4">
 
-  {{-- Columna izquierda = rail de miniaturas (2 filas) --}}
-  <div class="grid grid-rows-[auto_auto] gap-2 col-start-1 row-start-1">
-    {{-- Fila 1: primeras 6 miniaturas --}}
+  {{-- Rail de miniaturas (col 1) dividido en 2 filas --}}
+  <div class="col-start-1 row-start-1">
+    {{-- Fila 1: primeras 6 (una debajo de otra) --}}
     <div class="flex flex-col gap-2">
       @foreach ($top_ids as $id)
         <img
           src="{{ wp_get_attachment_image_url($id, 'thumbnail') }}"
-          class="w-16 h-16 object-cover cursor-pointer border border-white rounded bg-[#E1E6E4] hover:border-blue-500"
+          class="w-20 h-20 object-cover cursor-pointer border border-white rounded bg-[#E1E6E4] hover:border-blue-500"
           @click="$store.product.currentImage = '{{ wp_get_attachment_image_url($id, 'large') }}'">
       @endforeach
     </div>
+  </div>
 
-    {{-- Fila 2: resto de miniaturas (solo si hay) --}}
-    @if (count($bottom_ids))
+  {{-- Fila 2: resto de miniaturas (solo si hay más de 6) --}}
+  @if (count($bottom_ids))
+    <div class="col-start-1 row-start-2">
       <div class="flex flex-col gap-2">
         @foreach ($bottom_ids as $id)
           <img
             src="{{ wp_get_attachment_image_url($id, 'thumbnail') }}"
-            class="w-16 h-16 object-cover cursor-pointer border border-white rounded bg-[#E1E6E4] hover:border-blue-500"
+            class="w-20 h-20 object-cover cursor-pointer border border-white rounded bg-[#E1E6E4] hover:border-blue-500"
             @click="$store.product.currentImage = '{{ wp_get_attachment_image_url($id, 'large') }}'">
         @endforeach
       </div>
-    @endif
-  </div>
+    </div>
+  @endif
 
-  {{-- Columna derecha = imagen principal (ocupa 2 filas) --}}
+  {{-- Imagen principal (col 2, ocupa 2 filas) --}}
   <div
     class="relative col-start-2 row-span-2"
     x-data="{
@@ -63,12 +66,12 @@
 
     <div
       x-ref="zoom" x-show="showZoom && zoomEnabled" x-transition
-      class="absolute top-0 left-full ml-4 z-50 border rounded shadow-lg bg-white p-2 overflow-hidden"
-      style="width:700px;height:550px"
+      class="absolute top-0 left-full ml-4 z-50 border rounded shadow-lg bg-white p-2 overflow-hidden hidden md:block"
+      style="width: 700px; height: 550px;"
     >
       <img
         :src="$store.product.currentImage"
-        :style="'transform: scale(1.5) translate(' + zoomX + 'px,' + zoomY + 'px)'"
+        :style="'transform: scale(1.5) translate(' + zoomX + 'px, ' + zoomY + 'px)'"
         class="w-full h-auto object-contain"
         alt="Zoom imagen"
       >
