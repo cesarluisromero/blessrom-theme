cb46e71e93df67e9ebef09d782ef3d545cdfe02e
Cesar Luis RR <romcel87@gmail.com>
2025-08-21 19:40:14 -0500

    color

--- resources/views/partials/mobile-single-product.blade.php
+++ resources/views/partials/mobile-single-product.blade.php
@@ -1,42 +1,56 @@
 @php
-  // Construir color -> image_id desde variaciones
+  // Construir color -> URL de imagen de variación
   $color_image_map = [];
+  $variation_img_urls = [];
+
   if (!empty($available_variations)) {
     foreach ($available_variations as $v) {
       $color = $v['attributes']['attribute_pa_color'] ?? null;
+
+      // ID de imagen de la variación (distintos formatos según Woo)
       $imgId = $v['image_id'] ?? ($v['image']['id'] ?? null);
-      if ($color && $imgId) $color_image_map[$color] = (int) $imgId;
+
+      // URL 'large' de la imagen de la variación
+      $imgUrl = $imgId ? wp_get_attachment_image_url($imgId, 'large') : ($v['image']['url'] ?? null);
+
+      if ($color && $imgUrl) {
+        // 1) mapa color -> URL (para Alpine.store.product.colorImages)
+        if (!isset($color_image_map[$color])) {
+          $color_image_map[$color] = $imgUrl;
+        }
+        // 2) juntar todas las URLs de variación para meterlas en el carrusel móvil
+        $variation_img_urls[] = $imgUrl;
+      }
     }
   }
 
-  // Asegurar que TODAS las imágenes de variación estén en el slider
-  $variation_img_ids = array_values(array_unique(array_filter(array_map(function($v){
-    return $v['image_id'] ?? ($v['image']['id'] ?? null);
-  }, $available_variations ?? []))));
+  // Asegurar que el slider móvil contenga: imagen principal + adjuntas + variaciones
+  $base_img_urls = array_filter(array_map(function($id) {
+    return wp_get_attachment_image_url($id, 'large');
+  }, array_merge([$main_image], $attachment_ids)));
 
-  $ids = array_values(array_unique(array_merge([$main_image], $attachment_ids, $variation_img_ids)));
+  // Unir y limpiar duplicados por URL
+  $all_img_urls = array_values(array_unique(array_merge($base_img_urls, $variation_img_urls)));
 @endphp
 
 <script>
+  // Ahora el mapa es slug -> URL (igual que en desktop)
   window.BLESSROM_COLOR_IMAGE_MAP = {!! json_encode($color_image_map, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES) !!};
 </script>
+
 
 <div class="text-center text-lg font-semibold text-gray-800 lg:hidden mb-6">
         {{ $product->get_name() }}
 </div>
 <div x-data="productGallery()" class="product-swiper-movil swiper block lg:hidden mb-6">
-    
-    <div class="swiper-wrapper">        
-        @foreach ($ids as $id)
-            <div class="swiper-slide">
-            <img src="{{ wp_get_attachment_image_url($id, 'large') }}"
-                data-id="{{ $id }}"
-                class="w-full h-auto object-contain lg:hidden mb-6">
-            </div>
-        @endforeach
-    </div> 
-    
-    <div class="swiper-pagination absolute bottom-1 inset-x-0 flex justify-center"></div>   
+  <div class="swiper-wrapper">
+    @foreach ($all_img_urls as $url)
+      <div class="swiper-slide">
+        <img src="{{ $url }}" class="w-full h-auto object-contain lg:hidden mb-6">
+      </div>
+    @endforeach
+  </div>
+  <div class="swiper-pagination absolute bottom-1 inset-x-0 flex justify-center"></div>
 </div>
 <div class="px-4 py-4 space-y-3 lg:hidden mb-6">
     {{-- Precio --}}
