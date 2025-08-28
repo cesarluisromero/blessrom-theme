3354b908d3ce8a4cd37f61a25bcd27fd9fcf1778
Cesar Luis RR <romcel87@gmail.com>
2025-08-28 14:15:50 -0500

    rees

---
+++ resources/views/woocommerce/checkout/form-checkout.blade.php
@@ -0,0 +1,55 @@
+@php
+  // Asegura instancia
+  $checkout = isset($checkout) && is_object($checkout) ? $checkout : WC()->checkout();
+
+  // Hooks previos
+  do_action('woocommerce_before_checkout_form', $checkout);
+
+  // Bloquea si requiere registro y el usuario no está logueado
+  if (!$checkout->is_registration_enabled() && $checkout->is_registration_required() && !is_user_logged_in()) {
+      echo esc_html(apply_filters(
+          'woocommerce_checkout_must_be_logged_in_message',
+          __('You must be logged in to checkout.', 'woocommerce')
+      ));
+      return;
+  }
+@endphp
+
+<form
+  name="checkout"
+  method="post"
+  class="checkout woocommerce-checkout"
+  action="{{ esc_url( wc_get_checkout_url() ) }}"
+  enctype="multipart/form-data"
+  aria-label="{{ __('Checkout', 'woocommerce') }}"
+>
+  @if ($checkout->get_checkout_fields())
+    @php do_action('woocommerce_checkout_before_customer_details'); @endphp
+
+    <div class="col2-set" id="customer_details">
+      <div class="col-1">
+        @php do_action('woocommerce_checkout_billing'); @endphp
+      </div>
+
+      <div class="col-2">
+        @php do_action('woocommerce_checkout_shipping'); @endphp
+      </div>
+    </div>
+
+    @php do_action('woocommerce_checkout_after_customer_details'); @endphp
+  @endif
+
+  @php do_action('woocommerce_checkout_before_order_review_heading'); @endphp
+
+  <h3 id="order_review_heading">{{ __('Your order', 'woocommerce') }}</h3>
+
+  @php do_action('woocommerce_checkout_before_order_review'); @endphp
+
+  <div id="order_review" class="woocommerce-checkout-review-order">
+    @php do_action('woocommerce_checkout_order_review'); @endphp
+  </div>
+
+  @php do_action('woocommerce_checkout_after_order_review'); @endphp
+</form>
+
+@php do_action('woocommerce_after_checkout_form', $checkout); @endphp
