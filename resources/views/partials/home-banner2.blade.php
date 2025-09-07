1f01b559fac9ef8752a72c46ae4b1ac23180c376
Cesar Luis RR <romcel87@gmail.com>
2025-09-07 07:53:03 -0500

    b2

--- resources/views/partials/home-banner2.blade.php
+++ resources/views/partials/home-banner2.blade.php
@@ -14,7 +14,7 @@
           </div>
           <div class="swiper-slide">
             <img
-              src="{{ esc_url('https://blessrom.com/wp-content/uploads/2025/09/Neutral-Modern-Fashion-Collection-Banner-scaled.png') }}"
+              src="{{ esc_url('https://blessrom.com/wp-content/uploads/2025/09/Neutral-Modern-Fashion-Collection-Banner-2-scaled.png') }}"
               alt="Colección de vestidos"
               class="w-full h-full object-cover"
               loading="lazy" decoding="async">
