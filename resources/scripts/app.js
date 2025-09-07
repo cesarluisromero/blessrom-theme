3790162da6997adaf950fafb37c319c572f229c1
Cesar Luis RR <romcel87@gmail.com>
2025-09-07 09:53:50 -0500

    cat

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -403,7 +403,7 @@ document.addEventListener('DOMContentLoaded', () => {
     slidesPerView: 4,
     slidesPerGroup: 4,
     loop: true,
-    spaceBetween: 32,
+    spaceBetween: 30,
     navigation: { 
       nextEl: '.category-swiper-button-next',
       prevEl: '.category-swiper-button-prev',
@@ -430,8 +430,8 @@ document.addEventListener('DOMContentLoaded', () => {
         slidesPerGroup: 3,
       },
       1024: {
-        slidesPerView: 8,
-        slidesPerGroup: 8, 
+        slidesPerView: 6,
+        slidesPerGroup: 6, 
       },
     },
   });
