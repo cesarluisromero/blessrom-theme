9549a6ab43eadb81043d7eb4f6be19b8fe4232e5
Cesar Luis RR <romcel87@gmail.com>
2025-09-08 09:48:05 -0500

    banner

--- resources/views/partials/home-banner-polo-hombre.blade.php
+++ resources/views/partials/home-banner-polo-hombre.blade.php
@@ -29,9 +29,9 @@
 
         {{-- botón fuera del swiper --}}
         <div class="mt-6 mb-10 flex justify-center">
-          <a href="https://blessrom.com/tienda/?categorias[]=vestido"
+          <a href="https://blessrom.com/tienda/?categorias[]=hombre-polos"
             class="inline-flex items-center gap-2 rounded-full bg-[#FFB816] px-6 py-3 text-white font-semibold shadow hover:bg-yellow-500">
-            Ver más vestidos
+            Ver Todo
           </a>
         </div>
       </div>
