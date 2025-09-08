058f35c5f0187a5f49e490b30c05070b34d81e82
Cesar Luis RR <romcel87@gmail.com>
2025-09-08 10:04:59 -0500

    banner movil

--- resources/views/partials/home-banner-polo-hombre.blade.php
+++ resources/views/partials/home-banner-polo-hombre.blade.php
@@ -39,11 +39,40 @@
   </div>
 </section>
 
-<section class="block md:hidden w-full">
-  <div class="container mx-auto px-4">
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {!! do_shortcode('[smartslider3 slider="7"]') !!}
-  </div>
+<section class="block md:hidden full-bleed text-center py-6 overflow-x-clip">
+  <div class="bg-white">
+    <div class="swiper bannervestidos-swiper rounded-none" aria-label="Banner vestidos móvil">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img
+            src="{{ esc_url('https://blessrom.com/wp-content/uploads/2025/09/Creamy-Black-Clean-Mens-Fashion-Instagram-Post-movil.png') }}"
+            alt="Experimenta nuestra pasión por la moda en Tarapoto"
+            class="w-full h-auto block"
+            fetchpriority="high" decoding="async">
+        </div>
+        <div class="swiper-slide">
+          <img src="{{ esc_url('https://blessrom.com/wp-content/uploads/2025/09/Creamy-Black-Clean-Mens-Fashion-Instagram-Post-movil.png') }}"
+               alt="Colección de vestidos"
+               class="w-full h-auto block" loading="lazy" decoding="async">
+        </div>
+        
+      </div>
+
+      {{-- Flechas (ocultas en móvil) --}}
+      <div class="swiper-button-prev bannervestidos-swiper-button-prev !hidden md:!flex"></div>
+      <div class="swiper-button-next bannervestidos-swiper-button-next !hidden md:!flex"></div>
+      {{-- debajo del .swiper bannervestidos-swiper --}}
+    
+      {{-- botón fuera del swiper --}}
+      <div class="mt-6 mb-10 flex justify-center">
+        <a href="https://blessrom.com/tienda/?categorias[]=vestido"
+          class="inline-flex items-center gap-2 rounded-full bg-[#FFB816] px-6 py-3 text-white font-semibold shadow hover:bg-yellow-500">
+          Ver Todo
+        </a>
+      </div>
+    </div>
+    
+
   </div>
 </section>
 
